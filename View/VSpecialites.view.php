215c47e5490995df7152e4576ba95b8e490274e7
AlitaChu <l.n.scriptales@gmail.com>
2018-02-02 11:03:07 +0100

    Suppression du lien sur le nom de la spécialité en vue détails

--- View/VSpecialites.view.php
+++ View/VSpecialites.view.php
@@ -51,6 +51,51 @@ echo <<< HERE
 HERE;
      
     }
+    
+    public function showDetailsSpec($_data)
+    {
+        //debug($_data);
+        //echo 'avec id ='.$_GET['id'];
+        // Code html (vue) de la section 'spécialités' --->
+        
+        $ls_spe = '';        
+        foreach ($_data as $val)
+        {
+            $ls_spe.= '<div class="text-center large-2 medium-4 cell"><h3>'.$val['TITRE_ARTICLE'].'</h3>
+            <p><img src="../img/'.$val['IMAGE'].'" alt="'.$val['IMG_ALT'].'"/></p>
+        </div>';
+        }
+    
+        /* Exemple
+        <div class="text-center large-2 medium-4 cell">
+          <a href="">
+            <h3>Radiographie</h3>
+            <p><img src="../img/fullsize/radiographie.jpg" alt=""/></p>
+          </a>
+        </div> */
+
+echo <<< HERE
+<div class="white callout">
+  <div class="grid-container">
+    <div class="grid-x grid-padding-x">
+      <div class="large-10 medium-10 small-10 cell">
+          <h2> Notre engagement : des services de qualité à travers nos spécialités! </h2>
+          <p>Découvrez pour chacune de nos spécialité, notre panel de services, pour votre animal:</p>
+      </div> 
+      <div class="large-2 medium-2 small-2 cell">
+        <p><img src="" alt="Photo de Chat"/></p>
+      </div> 
+    </div> 
+    <div class="white callout grid-x grid-padding-x align-bottom">
+      $ls_spe
+    </div>  
+  </div>
+</div>
+
+
+HERE;
+     
+    }
 }
 
 ?>    
