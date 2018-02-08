399449333f29194a0ceb6a8b73c33222c4508dba
AlitaChu <l.n.scriptales@gmail.com>
2018-02-08 10:24:31 +0100

    Modif affichage vue avec séparateur et instanciation vue conseils

--- View/VConseils.view.php
+++ View/VConseils.view.php
@@ -1,20 +1,20 @@
 <?php
 
-class VSpecialites
+class VConseils
 {
     public function __construct(){}
     public function __destruct(){}
   
     
-    public function showSpecialites($_data)
+    public function showConseils($_data)
     {
         //debug($_data);
         // Code html (vue) de la section 'spécialités' --->
         
-        $ls_spe = '';        
+        $ls_con = '';        
         foreach ($_data as $val)
         {
-            $ls_spe.= '<div class="text-center large-2 medium-4 cell">
+            $ls_con.= '<div class="text-center large-2 medium-4 cell">
         <a href="../Php/index.php?ex=spe&id='.$val['ID_ARTICLE'].'"><h3>'.$val['TITRE_ARTICLE'].'</h3>
             <p><img src="../img/'.$val['IMAGE'].'" alt="'.$val['IMG_ALT'].'"/></p>
           </a>
@@ -42,7 +42,7 @@ echo <<< HERE
       </div> 
     </div> 
     <div class="white callout grid-x grid-padding-x align-bottom">
-      $ls_spe
+      $ls_con
     </div>  
   </div>
 </div>
@@ -52,16 +52,16 @@ HERE;
      
     }
     
-    public function showDetailsSpec($_data)
+    public function showDetailsCons($_data)
     {
         //debug($_data);
         //echo 'avec id ='.$_GET['id'];
         // Code html (vue) de la section 'spécialités' --->
         
-        $ls_spe = '';        
+        $ls_con = '';        
         foreach ($_data as $val)
         {
-            $ls_spe.= '<div class="text-center large-4 medium-4 cell">
+            $ls_con.= '<div class="text-center large-4 medium-4 cell">
             <p><img src="../img/'.$val['IMAGE'].'" alt="'.$val['IMG_ALT'].'"/></p>
             </div>
             <div class="large-8 medium-8 cell">
@@ -84,15 +84,15 @@ echo <<< HERE
   <div class="grid-container">
     <!--<div class="grid-x grid-padding-x">
       <div class="large-10 medium-10 small-10 cell">
-          <h2> Notre engagement : des services de qualité à travers nos spécialités! </h2>
-          <p>Découvrez pour chacune de nos spécialité, notre panel de services, pour votre animal:</p>
+          <h2> Nos conseils pratiques </h2>
+          <p>Découvrez pour chaque situation, voici nos conseils:</p>
       </div>
       <div class="large-2 medium-2 small-2 cell">
         <p><img src="" alt="Photo de Chat"/></p>
       </div> 
     </div> -->
     <div class="white callout grid-x grid-padding-x">
-      $ls_spe
+      $ls_con
     </div>  
   </div>
 </div>
